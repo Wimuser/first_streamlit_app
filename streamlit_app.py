fb5c1235bea308a0dcd44fbcbc9cf7b69cc75198
Wimuser <144745397+Wimuser@users.noreply.github.com>
2023-09-12 11:43:53 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('my parents new healthy dinner')
+
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale, spincacj & rocket smoothie')
+streamlit.text('hard-boiled gree-reange egg')
