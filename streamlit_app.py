4688e04f7d8b39d721f1d379380b681de93c42df
Wimuser <144745397+Wimuser@users.noreply.github.com>
2023-09-12 11:47:06 +1200

    header 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣 omega 3 & blueberry oatmeal')
 streamlit.text('🥗 kale, spincacj & rocket smoothie')
 streamlit.text('🐔 hard-boiled gree-reange egg')
 streamlit.text('🥑🍞 advocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
