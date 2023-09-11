03f8528d03f09704c6625c4ae3ebdff689c3bbb3
Wimuser <144745397+Wimuser@users.noreply.github.com>
2023-09-12 11:44:59 +1200

    add header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('my parents new healthy dinner')
-
+streamlit.header('Breakfast Menu')
 streamlit.text('omega 3 & blueberry oatmeal')
 streamlit.text('kale, spincacj & rocket smoothie')
 streamlit.text('hard-boiled gree-reange egg')
