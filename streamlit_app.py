6dbe91acea216ee4b8bd8032918dc270764d78cd
Wimuser <144745397+Wimuser@users.noreply.github.com>
2023-09-15 11:46:20 +1200

    advice in a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,10 @@ streamlit.header('Fuityvice Fruite Advice')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# format inot a new table
+streamlit.dataframe(fruityvice_normalized)
+
