c215a6bc14ff5be4d2f8035e16ba5e1a4ee13282
Wimuser <144745397+Wimuser@users.noreply.github.com>
2023-09-15 13:48:16 +1200

    try statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,16 +25,26 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display api response
 streamlit.header('Fuityvice Fruite Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # format inot a new table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
+
+ #streamlit.write('The user entered ', fruit_choice)
 #streamlit.text(fruityvice_response.json())
-
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# format inot a new table
-streamlit.dataframe(fruityvice_normalized)
+
+
 
 # dont run anything past here
 streamlit.stop()
